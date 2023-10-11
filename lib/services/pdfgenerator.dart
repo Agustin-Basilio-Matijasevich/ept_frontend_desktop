6d9ec6fcf50e89edbbfdcb19b14698fafa1a2295
Basilio Matijasevich, Agustín <agustinbasiliomatijasevich@gmail.com>
2023-10-10 21:22:37 -0300

    PDF

---
+++ lib/services/pdfgenerator.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:ept_frontend/models/usuario.dart';
+
+class PDFGenerator{
+  static Future<bool> listarAlumnosPorCursoPDF(List<Usuario> listaUsuarios, String rutaSalida) async {
+    final pdf = pw.Document();
+    final file = File(rutaSalida);
+
+    if (listaUsuarios.isEmpty){
+      return false;
+    }
+
+    //Añade Paginas
+
+    try{
+      await file.writeAsBytes(await pdf.save());
+    }
+    catch (e){
+      print("Error escribiendo archivo. Exeption: $e");
+      return false;
+    }
+
+    return true;
+  }
+}
